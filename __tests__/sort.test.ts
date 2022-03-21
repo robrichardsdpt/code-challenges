237b522b575ce7a4c5875a8e524f3f053259f712
Rob <robrichardsdpt@gmail.com>
2022-03-21 16:16:33 -0400

    added mergeSort and tests

---
+++ __tests__/sort.test.ts
@@ -0,0 +1,19 @@
+import { mergeSort } from "../sort";
+
+describe("testing mergeSort", () => {
+  test("mergeSort takes array and number present returning true", () => {
+    expect(mergeSort([9, 6, 5, 4, 2, 0, 8])).toStrictEqual([
+      0, 2, 4, 5, 6, 8, 9,
+    ]);
+  });
+
+  test("mergeSort takes array and number not present returning false", () => {
+    expect(mergeSort([5, 8, 2, 9, -5, 6, 0, 4])).toStrictEqual([
+      -5, 0, 2, 4, 5, 6, 8, 9,
+    ]);
+  });
+
+  test("mergeSort takes empty array and returns false", () => {
+    expect(mergeSort([])).toStrictEqual([]);
+  });
+});
---
+++ sort.ts
@@ -0,0 +1,28 @@
+const merge = (left: Array<number>, right: Array<number>): Array<number> => {
+  let arr = [];
+  // Break out of loop if any one of the array gets empty
+  while (left.length && right.length) {
+    // Pick the smaller among the smallest element of left and right sub arrays
+    if (left[0] < right[0]) {
+      arr.push(left.shift());
+    } else {
+      arr.push(right.shift());
+    }
+  }
+
+  // Concatenating the leftover elements
+  // (in case we didn't go through the entire left or right array)
+  return [...arr, ...left, ...right];
+};
+
+export const mergeSort = (arr: Array<number>): Array<number> => {
+  const midpoint = arr.length / 2;
+
+  // Base case or terminating case
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  const left = arr.splice(0, midpoint);
+  return merge(mergeSort(left), mergeSort(arr));
+};
