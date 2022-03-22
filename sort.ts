ec71f84450487ab8b87e8945db3b384a4df4d860
Rob <robrichardsdpt@gmail.com>
2022-03-22 10:03:52 -0400

    fixed bubble sort

--- sort.ts
+++ sort.ts
@@ -28,7 +28,7 @@ export const mergeSort = (arr: Array<number>): Array<number> => {
 };
 
 export const bubbleSort = (arr: Array<number>): Array<number> => {
-  for (let i = 0; i < arr.length - 1; i++) {
+  for (let i = 0; i < arr.length; i++) {
     for (let j = 0; j < arr.length; j++) {
       if (arr[j] > arr[j + 1]) {
         let temp = arr[j];
