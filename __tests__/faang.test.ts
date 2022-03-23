e81fa56cad9eca620f50c0ee37b91636b92517c2
Rob <robrichardsdpt@gmail.com>
2022-03-23 10:23:38 -0400

    2 pointer system used for matching two typed strings

--- __tests__/faang.test.ts
+++ __tests__/faang.test.ts
@@ -1,4 +1,4 @@
-import { matchTwoTypedStrings } from "../faang";
+import { matchTwoTypedStrings, matchTwoTypedStringsAlt } from "../faang";
 
 describe("testing binarySearch", () => {
   test("matchTwoTypedStrings takes 2 strings with backspace but are equal returning true", () => {
@@ -27,5 +27,35 @@ describe("testing binarySearch", () => {
 
   test("matchTwoTypedStrings takes 2 strings with backspace that leave empty returning true", () => {
     expect(matchTwoTypedStrings("##", "c#")).toBe(true);
+  });
+});
+
+describe("testing matchTwoTypedStringsAlt", () => {
+  test("matchTwoTypedStringsAlt takes 2 strings with backspace but are equal returning true", () => {
+    expect(matchTwoTypedStringsAlt("hello", "hee#llo")).toBe(true);
+  });
+
+  test("matchTwoTypedStringsAlt takes 2 strings with backspace but are not equal returning false", () => {
+    expect(matchTwoTypedStringsAlt("hello", "hee#ll#o")).toBe(false);
+  });
+
+  test("matchTwoTypedStringsAlt takes 2 strings with backspace and mixed letters/numbers that are not equal returning false", () => {
+    expect(matchTwoTypedStringsAlt("hello", "h3e#llo")).toBe(false);
+  });
+
+  test("matchTwoTypedStringsAlt takes 2 strings with different cases but are equal returning false", () => {
+    expect(matchTwoTypedStringsAlt("hello", "Hello")).toBe(false);
+  });
+
+  test("matchTwoTypedStringsAlt takes 2 mixed strings with backspace but are equal returning true", () => {
+    expect(matchTwoTypedStringsAlt("32a1", "32ab#1")).toBe(true);
+  });
+
+  test("matchTwoTypedStringsAlt takes 2 strings with backspace that leave empty returning true", () => {
+    expect(matchTwoTypedStringsAlt("ab##", "c#")).toBe(true);
+  });
+
+  test("matchTwoTypedStringsAlt takes 2 strings with backspace that leave empty returning true", () => {
+    expect(matchTwoTypedStringsAlt("##", "c#")).toBe(true);
   });
 });
--- faang.ts
+++ faang.ts
@@ -16,3 +16,43 @@ const handleHashes = (string: string): string => {
 export const matchTwoTypedStrings = (str1: string, str2: string): boolean => {
   return handleHashes(str1) === handleHashes(str2);
 };
+
+export const matchTwoTypedStringsAlt = (
+  str1: string,
+  str2: string
+): boolean => {
+  let p1 = str1.length - 1,
+    p2 = str2.length - 1;
+
+  while (p1 >= 0 || p2 >= 0) {
+    if (str1[p1] === "#" || str2[p2] === "#") {
+      if (str1[p1] === "#") {
+        let backcount = 2;
+        while (backcount > 0) {
+          p1--;
+          backcount--;
+          if (str1[p1] === "#") {
+            backcount += 2;
+          }
+        }
+      }
+      if (str2[p2] === "#") {
+        let backcount = 2;
+        while (backcount > 0) {
+          p2--;
+          backcount--;
+          if (str2[p2] === "#") {
+            backcount += 2;
+          }
+        }
+      }
+    } else {
+      if (str1[p1] !== str2[p2]) {
+        return false;
+      }
+      p1--;
+      p2--;
+    }
+  }
+  return true;
+};
