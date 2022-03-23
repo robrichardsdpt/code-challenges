62cca84f46b3fc4c61fc2c08433b8fd06dffd198
Rob <robrichardsdpt@gmail.com>
2022-03-23 09:23:23 -0400

    updated descriptions for tests

--- __tests__/faang.test.ts
+++ __tests__/faang.test.ts
@@ -1,27 +1,31 @@
 import { matchTwoTypedStrings } from "../faang";
 
 describe("testing binarySearch", () => {
-  test("matchTwoTypedStrings takes array and number present returning true", () => {
+  test("matchTwoTypedStrings takes 2 strings with backspace but are equal returning true", () => {
     expect(matchTwoTypedStrings("hello", "hee#llo")).toBe(true);
   });
 
-  test("binarySearch takes array and number not present returning false", () => {
+  test("matchTwoTypedStrings takes 2 strings with backspace but are not equal returning false", () => {
     expect(matchTwoTypedStrings("hello", "hee#ll#o")).toBe(false);
   });
 
-  test("binarySearch takes array and number not present returning false", () => {
+  test("matchTwoTypedStrings takes 2 strings with backspace and mixed letters/numbers that are not equal returning false", () => {
     expect(matchTwoTypedStrings("hello", "h3e#llo")).toBe(false);
   });
 
-  test("binarySearch takes array and number not present returning false", () => {
+  test("matchTwoTypedStrings takes 2 strings with different cases but are equal returning false", () => {
     expect(matchTwoTypedStrings("hello", "Hello")).toBe(false);
   });
 
-  test("binarySearch takes array and number not present returning false", () => {
+  test("matchTwoTypedStrings takes 2 mixed strings with backspace but are equal returning true", () => {
     expect(matchTwoTypedStrings("32a1", "32ab#1")).toBe(true);
   });
 
-  test("binarySearch takes array and negative number outside of values returning false", () => {
+  test("matchTwoTypedStrings takes 2 strings with backspace that leave empty returning true", () => {
     expect(matchTwoTypedStrings("ab##", "c#")).toBe(true);
+  });
+
+  test("matchTwoTypedStrings takes 2 strings with backspace that leave empty returning true", () => {
+    expect(matchTwoTypedStrings("##", "c#")).toBe(true);
   });
 });
