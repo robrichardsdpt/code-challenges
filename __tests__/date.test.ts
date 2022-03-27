8bf538b289c7344d7c7e929e54b6cfa322cc9f09
Rob <robrichardsdpt@gmail.com>
2022-03-27 17:49:31 -0400

    added employeeResponsibilityPercentage function and tests

--- __tests__/date.test.ts
+++ __tests__/date.test.ts
@@ -2,6 +2,10 @@ import {
   isDateRangeInRange,
   wasItTheWeekend,
   didThePersonHaveOff,
+  employeeResponsibilityPercentage,
+  EmployeeResponsibilityObj,
+  ResponsibilityObj,
+  DateRangeObject,
 } from "../date";
 
 describe("isDateRangeInRange tests", () => {
@@ -145,5 +149,151 @@ describe("didThePersonHaveOff tests", () => {
   });
   test("didThePersonHaveOff finds Saturday based on date and returns true", () => {
     expect(didThePersonHaveOff("4/2/2022", daysOff)).toBe(true);
+  });
+});
+
+const dateInterval: DateRangeObject = {
+  startDate: new Date("1/15/2022"),
+  endDate: new Date("1/16/2022"),
+};
+
+const temporaryResponsibilities: Array<ResponsibilityObj> = [
+  {
+    dateInterval: {
+      startDate: new Date("1/15/2022"),
+      endDate: new Date("1/15/2022"),
+    },
+    coaching: 67,
+    admin: 33,
+  },
+  {
+    dateInterval: {
+      startDate: new Date("1/17/2022"),
+      endDate: new Date("1/30/2022"),
+    },
+    coaching: 80,
+    admin: 20,
+  },
+  {
+    dateInterval: {
+      startDate: new Date("2/15/2022"),
+      endDate: new Date("2/15/2022"),
+    },
+    coaching: 50,
+    admin: 50,
+  },
+];
+
+const employeeObject: EmployeeResponsibilityObj = {
+  name: "Great Coach",
+  baselineResponsibility: {
+    dateInterval: {
+      startDate: new Date("1/1/2022"),
+      endDate: null,
+    },
+    coaching: 75,
+    admin: 25,
+  },
+  temporary: [],
+};
+
+describe("employeeResponsibilityPercentage tests", () => {
+  test("employeeResponsibilityPercentage finds 33% admin/67% coaching based on dates provided with no temp values for coach", () => {
+    const results = {
+      dateInterval: dateInterval,
+      coaching: employeeObject.baselineResponsibility.coaching,
+      admin: employeeObject.baselineResponsibility.admin,
+    };
+    expect(
+      employeeResponsibilityPercentage(employeeObject, dateInterval)
+    ).toStrictEqual(results);
+  });
+
+  test("employeeResponsibilityPercentage finds 26% admin/74% coaching based on dates provided  with temp list", () => {
+    employeeObject.temporary = temporaryResponsibilities;
+    const results = {
+      dateInterval: {
+        startDate: new Date("1/15/2022"),
+        endDate: new Date("1/17/2022"),
+      },
+      coaching: 74,
+      admin: 26,
+    };
+    expect(
+      employeeResponsibilityPercentage(employeeObject, {
+        startDate: new Date("1/15/2022"),
+        endDate: new Date("1/17/2022"),
+      })
+    ).toStrictEqual(results);
+  });
+
+  test("employeeResponsibilityPercentage finds 29% admin/71% coaching based on dates provided with one temp range", () => {
+    employeeObject.temporary = temporaryResponsibilities;
+    const results = {
+      dateInterval: dateInterval,
+      coaching: 71,
+      admin: 29,
+    };
+    expect(
+      employeeResponsibilityPercentage(employeeObject, dateInterval)
+    ).toStrictEqual(results);
+  });
+
+  test("employeeResponsibilityPercentage finds 50% admin/50% coaching based on dates provided one temp range for one day and therefore baseline not factored", () => {
+    employeeObject.temporary = temporaryResponsibilities;
+    const results = {
+      dateInterval: {
+        startDate: new Date("2/15/2022"),
+        endDate: new Date("2/15/2022"),
+      },
+      coaching: 50,
+      admin: 50,
+    };
+
+    expect(
+      employeeResponsibilityPercentage(employeeObject, {
+        startDate: new Date("2/15/2022"),
+        endDate: new Date("2/15/2022"),
+      })
+    ).toStrictEqual(results);
+  });
+
+  test("employeeResponsibilityPercentage finds 0% admin/0% coaching based on dates provided not falling in range of employment", () => {
+    employeeObject.temporary = temporaryResponsibilities;
+    const results = {
+      dateInterval: {
+        startDate: new Date("12/15/2021"),
+        endDate: new Date("12/15/2021"),
+      },
+      coaching: 0,
+      admin: 0,
+    };
+    expect(
+      employeeResponsibilityPercentage(employeeObject, {
+        startDate: new Date("12/15/2021"),
+        endDate: new Date("12/15/2021"),
+      })
+    ).toStrictEqual(results);
+  });
+
+  test("employeeResponsibilityPercentage finds 0% admin/0% coaching based on dates provided not falling in range of employment", () => {
+    employeeObject.temporary = temporaryResponsibilities;
+    employeeObject.baselineResponsibility.dateInterval.endDate = new Date(
+      "3/23/2022"
+    );
+    const results = {
+      dateInterval: {
+        startDate: new Date("3/25/2022"),
+        endDate: new Date("3/29/2022"),
+      },
+      coaching: 0,
+      admin: 0,
+    };
+    expect(
+      employeeResponsibilityPercentage(employeeObject, {
+        startDate: new Date("3/25/2022"),
+        endDate: new Date("3/29/2022"),
+      })
+    ).toStrictEqual(results);
   });
 });
--- date.ts
+++ date.ts
@@ -7,11 +7,11 @@ export const isDateRangeInRange = (
   storedDateObj: DateRangeObject,
   userDateObj: DateRangeObject
 ): boolean => {
-  if (userDateObj.endDate.getTime() <= storedDateObj.startDate.getTime()) {
+  if (userDateObj.endDate.getTime() < storedDateObj.startDate.getTime()) {
     return false;
   }
   if (
-    userDateObj.startDate.getTime() >=
+    userDateObj.startDate.getTime() >
     (storedDateObj.endDate ? storedDateObj.endDate.getTime() : Date.now())
   ) {
     return false;
@@ -37,4 +37,78 @@ export const didThePersonHaveOff = (
   return false;
 };
 
+export type ResponsibilityObj = {
+  dateInterval: DateRangeObject;
+  coaching: number;
+  admin: number;
+};
+export type EmployeeResponsibilityObj = {
+  name: string;
+  baselineResponsibility: ResponsibilityObj;
+  temporary: Array<ResponsibilityObj>;
+};
+
+const getTotalTimeInRange = (dateRange: DateRangeObject): number => {
+  return (
+    (dateRange.endDate.getTime() - dateRange.startDate.getTime()) / 86400000 + 1
+  );
+};
+
 // percent of time worked in interval problem
+export const employeeResponsibilityPercentage = (
+  employeeObject: EmployeeResponsibilityObj,
+  inputDateRange: DateRangeObject
+): ResponsibilityObj => {
+  if (employeeObject.temporary.length === 0) {
+    return {
+      ...employeeObject.baselineResponsibility,
+      dateInterval: inputDateRange,
+    };
+  }
+  const { baselineResponsibility, temporary } = employeeObject;
+
+  baselineResponsibility.dateInterval.endDate = baselineResponsibility
+    .dateInterval.endDate
+    ? baselineResponsibility.dateInterval.endDate
+    : new Date();
+
+  if (
+    !isDateRangeInRange(baselineResponsibility.dateInterval, inputDateRange)
+  ) {
+    return {
+      dateInterval: inputDateRange,
+      coaching: 0,
+      admin: 0,
+    };
+  }
+  const totalTimeInInterval = getTotalTimeInRange(inputDateRange);
+  let totalTimeInBaselineResponsibility = totalTimeInInterval,
+    totalCoachingResponsibilityPerDay = 0,
+    totalAdminResponsibilityPerDay = 0;
+
+  temporary.forEach((item) => {
+    const { dateInterval, coaching, admin } = item;
+    if (isDateRangeInRange(inputDateRange, dateInterval)) {
+      dateInterval.endDate =
+        dateInterval.endDate.getDate() <= inputDateRange.endDate.getDate()
+          ? dateInterval.endDate
+          : inputDateRange.endDate;
+      const timeInTemporaryResponsibility = getTotalTimeInRange(dateInterval);
+      totalTimeInBaselineResponsibility -= timeInTemporaryResponsibility;
+      totalCoachingResponsibilityPerDay +=
+        coaching * timeInTemporaryResponsibility;
+      totalAdminResponsibilityPerDay += admin * timeInTemporaryResponsibility;
+    }
+  });
+
+  totalCoachingResponsibilityPerDay +=
+    baselineResponsibility.coaching * totalTimeInBaselineResponsibility;
+  totalAdminResponsibilityPerDay +=
+    baselineResponsibility.admin * totalTimeInBaselineResponsibility;
+
+  return {
+    dateInterval: inputDateRange,
+    coaching: totalCoachingResponsibilityPerDay / totalTimeInInterval,
+    admin: totalAdminResponsibilityPerDay / totalTimeInInterval,
+  };
+};
