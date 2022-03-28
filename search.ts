4df3e7596c6dafa853828b1cf17141b7edc39089
Rob <robrichardsdpt@gmail.com>
2022-03-28 11:27:58 -0400

    refactored binary search;

--- search.ts
+++ search.ts
@@ -2,15 +2,14 @@ export const binarySearch = (
   sortedArr: Array<number>,
   numberToFind: number
 ): boolean => {
-  let start = 0;
-  let end = sortedArr.length - 1;
+  let start = 0,
+    end = sortedArr.length - 1;
 
   while (start <= end) {
-    let mid = Math.floor((start + end) / 2);
+    let mid = Math.floor((end + start) / 2);
     if (sortedArr[mid] === numberToFind) return true;
-    else if (sortedArr[mid] < numberToFind) start = mid + 1;
-    else end = mid - 1;
+    if (numberToFind < sortedArr[mid]) end = mid - 1;
+    else start = mid + 1;
   }
-
   return false;
 };
