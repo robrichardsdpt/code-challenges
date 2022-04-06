9e2c2aa1258160ff18b157ed443a9b8b9536da86
Rob <robrichardsdpt@gmail.com>
2022-04-06 12:10:58 -0400

    fixed nit

--- index.ts
+++ index.ts
@@ -351,7 +351,7 @@ export const roundToNextHalf = (num: number): number => {
 
 export const likeString = (arr: Array<string>): string => {
   if (arr.length === 0) return "no one likes this";
-  if (arr.length === 1) return `${arr[0]} like this`;
+  if (arr.length === 1) return `${arr[0]} likes this`;
   if (arr.length === 2) return `${arr[0]} and ${arr[1]} like this`;
   if (arr.length === 3) return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
   return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
