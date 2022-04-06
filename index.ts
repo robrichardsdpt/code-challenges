32357ea377e1de61166ad680e99a6fdf040484ca
Rob <robrichardsdpt@gmail.com>
2022-04-06 12:06:59 -0400

    likeString function implemented

--- index.ts
+++ index.ts
@@ -348,3 +348,13 @@ export const roundToNextHalf = (num: number): number => {
 // ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
 // ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
 // ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"
+
+export const likeString = (arr: Array<string>): string => {
+  if (arr.length === 0) return "no one likes this";
+  if (arr.length === 1) return `${arr[0]} like this`;
+  if (arr.length === 2) return `${arr[0]} and ${arr[1]} like this`;
+  if (arr.length === 3) return `${arr[0]}, ${arr[1]} and ${arr[2]} like this`;
+  return `${arr[0]}, ${arr[1]} and ${arr.length - 2} others like this`;
+};
+
+console.log(likeString(["Alex", "Jacob", "Mark", "Max"]));
