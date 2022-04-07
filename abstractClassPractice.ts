f61bc027855bb5208c01358ea154d28aa2bc5795
Rob <robrichardsdpt@gmail.com>
2022-04-07 14:36:13 -0400

    added instanceof conditional

--- abstractClassPractice.ts
+++ abstractClassPractice.ts
@@ -10,9 +10,22 @@ abstract class Animal {
       `your dog, ${this.name}, just ate the ${this.eats} they were fed`
     );
   }
+  sayHi() {
+    console.log(
+      `You said hi to ${this.name}, and ${
+        this instanceof Dog ? "their tail wagged" : "they purred"
+      }`
+    );
+  }
 }
 
 class Dog extends Animal {
+  constructor(eats: string, name: string) {
+    super(eats, name);
+  }
+}
+
+class Cat extends Animal {
   constructor(eats: string, name: string) {
     super(eats, name);
   }
@@ -23,3 +36,7 @@ console.log(dog.eats);
 console.log(dog.eat());
 const fido = new Dog("carrots", "fido");
 console.log(fido.eat());
+console.log(fido.sayHi());
+const cat = new Cat("fish", "garfield");
+console.log(cat.eat());
+console.log(cat.sayHi());
