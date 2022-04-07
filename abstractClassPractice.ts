c61c79c14fa9b6d2bbc5d3a8420b84955273601c
Rob <robrichardsdpt@gmail.com>
2022-04-07 14:22:48 -0400

    added this.name

--- abstractClassPractice.ts
+++ abstractClassPractice.ts
@@ -1,21 +1,25 @@
 abstract class Animal {
   eats: string = "";
-  constructor(eats: string) {
+  name: string = "";
+  constructor(eats: string, name: string) {
     this.eats = eats;
+    this.name = name;
   }
   eat() {
-    console.log(`your dog just ate the ${this.eats} they were fed`);
+    console.log(
+      `your dog, ${this.name}, just ate the ${this.eats} they were fed`
+    );
   }
 }
 
 class Dog extends Animal {
-  constructor(eats: string) {
-    super(eats);
+  constructor(eats: string, name: string) {
+    super(eats, name);
   }
 }
 
-const dog = new Dog("meat");
+const dog = new Dog("meat", "harry");
 console.log(dog.eats);
 console.log(dog.eat());
-const fido = new Dog("carrots");
+const fido = new Dog("carrots", "fido");
 console.log(fido.eat());
