1e3f6b94be8075a4ba2239f179dd0673d4b14352
Rob <robrichardsdpt@gmail.com>
2022-04-12 21:00:16 -0400

    added next exercise.

--- index.ts
+++ index.ts
@@ -438,3 +438,20 @@ export const maxSequence = (array: Array<number>): number => {
 
   return largestSum;
 };
+
+// Your message is a string containing space separated words.
+// You need to encrypt each word in the message using the following rules:
+// The first letter must be converted to its ASCII code.
+// The second letter must be switched with the last letter
+// Keepin' it simple: There are no special characters in the input.
+
+const encryptThis = (text) => {
+  // Implement me! :)
+};
+
+//    assert.strictEqual(encryptThis("A"), "65");
+// assert.strictEqual(encryptThis("A wise old owl lived in an oak"), "65 119esi 111dl 111lw 108dvei 105n 97n 111ka");
+// assert.strictEqual(encryptThis("The more he saw the less he spoke"), "84eh 109ero 104e 115wa 116eh 108sse 104e 115eokp");
+// assert.strictEqual(encryptThis("The less he spoke the more he heard"), "84eh 108sse 104e 115eokp 116eh 109ero 104e 104dare");
+// assert.strictEqual(encryptThis("Why can we not all be like that wise old bird"), "87yh 99na 119e 110to 97ll 98e 108eki 116tah 119esi 111dl 98dri");
+// assert.strictEqual(encryptThis("Thank you Piotr for all your help"), "84kanh 121uo 80roti 102ro 97ll 121ruo 104ple");
