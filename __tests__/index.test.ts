41f21f469c4a1608531ebc35257be1ffafaae2c5
Rob <robrichardsdpt@gmail.com>
2022-04-14 09:39:00 -0400

    added validatePin tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -26,6 +26,7 @@ import {
   isSquare,
   DNAStrand,
   binaryArrayToNumber,
+  validatePIN,
 } from "../index";
 
 describe("testing roundToNext5", () => {
@@ -596,5 +597,35 @@ describe("binaryArrayToNumber tests", () => {
   });
   test("binaryArrayToNumber receives array and returns appropriate value", () => {
     expect(binaryArrayToNumber([])).toEqual(0);
+  });
+});
+
+describe("validatePIN", function () {
+  it("should return False for pins with length other than 4 or 6", function () {
+    expect(validatePIN("1")).toBe(false);
+    expect(validatePIN("12")).toBe(false);
+    expect(validatePIN("123")).toBe(false);
+    expect(validatePIN("12345")).toBe(false);
+    expect(validatePIN("1234567")).toBe(false);
+    expect(validatePIN("-1234")).toBe(false);
+    expect(validatePIN("1.234")).toBe(false);
+    expect(validatePIN("-1.234")).toBe(false);
+    expect(validatePIN("00000000")).toBe(false);
+  });
+
+  it("should return False for pins which contain characters other than digits", function () {
+    expect(validatePIN("a234")).toBe(false);
+    expect(validatePIN(".234")).toBe(false);
+  });
+
+  it("should return True for valid pins", function () {
+    expect(validatePIN("1234")).toBe(true);
+    expect(validatePIN("0000")).toBe(true);
+    expect(validatePIN("1111")).toBe(true);
+    expect(validatePIN("123456")).toBe(true);
+    expect(validatePIN("098765")).toBe(true);
+    expect(validatePIN("000000")).toBe(true);
+    expect(validatePIN("123456")).toBe(true);
+    expect(validatePIN("090909")).toBe(true);
   });
 });
