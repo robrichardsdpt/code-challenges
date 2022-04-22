b69dc2a5ed8a3d3afe85144c074e8f2453c9e1fc
Rob <robrichardsdpt@gmail.com>
2022-04-22 13:29:29 -0400

    betterThanAverage tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -39,6 +39,7 @@ import {
   reverseWords,
   countPositivesSumNegatives,
   isIsogram,
+  betterThanAverage,
 } from "../index";
 
 describe("testing roundToNext5", () => {
@@ -824,5 +825,35 @@ describe("isIsogram tests", () => {
   });
   test("isIsogram takes ''", () => {
     expect(isIsogram("")).toBe(true);
+  });
+});
+
+describe("betterThanAverage Tests", function () {
+  test("betterThanAverage([2, 3], 5) should return true", function () {
+    expect(betterThanAverage([2, 3], 5)).toEqual(true);
+  });
+
+  test("betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75) should return true", function () {
+    expect(betterThanAverage([100, 40, 34, 57, 29, 72, 57, 88], 75)).toEqual(
+      true
+    );
+  });
+
+  test("betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 69) should return true", function () {
+    expect(betterThanAverage([12, 23, 34, 45, 56, 67, 78, 89, 90], 69)).toEqual(
+      true
+    );
+  });
+
+  test("betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50) should return false", function () {
+    expect(betterThanAverage([41, 75, 72, 56, 80, 82, 81, 33], 50)).toEqual(
+      false
+    );
+  });
+
+  test("betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28], 21) should return false", function () {
+    expect(betterThanAverage([29, 55, 74, 60, 11, 90, 67, 28], 21)).toEqual(
+      false
+    );
   });
 });
