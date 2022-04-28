9e8bb18f4fddddc0efeb2498ad1ed1a9c3aa7d55
Rob <robrichardsdpt@gmail.com>
2022-04-28 18:14:21 -0400

    added tests for getGrade

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -55,6 +55,7 @@ import {
   sortDigitString,
   doubleChar,
   alphabetPosition,
+  getGrade,
 } from "../index";
 
 describe("testing roundToNext5", () => {
@@ -1177,4 +1178,34 @@ describe("Randomly generated tests:", () => {
     }
     it(`Testing "${x}"`, () => expect(alphabetPosition(x)).toBe(ap(x)));
   }
+});
+
+describe("getGrades test", () => {
+  it("should return an A", function () {
+    expect(getGrade(95, 90, 93)).toBe("A");
+    expect(getGrade(100, 85, 96)).toBe("A");
+    expect(getGrade(92, 93, 94)).toBe("A");
+    expect(getGrade(100, 100, 100)).toBe("A");
+  });
+  it("should return a B", function () {
+    expect(getGrade(70, 70, 100)).toBe("B");
+    expect(getGrade(82, 85, 87)).toBe("B");
+    expect(getGrade(84, 79, 85)).toBe("B");
+  });
+  it("should return a C", function () {
+    expect(getGrade(70, 70, 70)).toBe("C");
+    expect(getGrade(75, 70, 79)).toBe("C");
+    expect(getGrade(60, 82, 76)).toBe("C");
+  });
+  it("should return a D", function () {
+    expect(getGrade(65, 70, 59)).toBe("D");
+    expect(getGrade(66, 62, 68)).toBe("D");
+    expect(getGrade(58, 62, 70)).toBe("D");
+  });
+  it("should return an F", function () {
+    expect(getGrade(44, 55, 52)).toBe("F");
+    expect(getGrade(48, 55, 52)).toBe("F");
+    expect(getGrade(58, 59, 60)).toBe("F");
+    expect(getGrade(0, 0, 0)).toBe("F");
+  });
 });
