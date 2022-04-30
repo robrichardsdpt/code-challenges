077e57f2ef815211a490148b9f46213049f34ab6
Rob <robrichardsdpt@gmail.com>
2022-04-29 21:05:18 -0400

    added tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -1,4 +1,3 @@
-import { convertToObject } from "typescript";
 import {
   removeUrlAnchor,
   sumDigits,
@@ -61,6 +60,7 @@ import {
   expandedForm,
   accum,
   bmi,
+  getVowelCount,
 } from "../index";
 
 describe("testing roundToNext5", () => {
@@ -1320,5 +1320,27 @@ describe("bmi tests", () => {
     expect(bmi(80, 1.8)).toEqual("Normal");
     expect(bmi(90, 1.8)).toEqual("Overweight");
     expect(bmi(110, 1.8)).toEqual("Obese");
+  });
+});
+
+describe("getVowelCount(Tests", function () {
+  it("should return 5 for 'abracadabra'", function () {
+    expect(getVowelCount("abracadabra")).toBe(5);
+  });
+  it("should return 4 for 'pear tree'", function () {
+    expect(getVowelCount("pear tree")).toBe(4);
+  });
+  it("should return 13 for 'o a kak ushakov lil vo kashu kakao'", function () {
+    expect(getVowelCount("o a kak ushakov lil vo kashu kakao")).toBe(13);
+  });
+  it("should return 0 for 'my pyx'", function () {
+    expect(getVowelCount("my pyx")).toBe(0);
+  });
+  it("should return 168 for a long input", function () {
+    expect(
+      getVowelCount(
+        "tk r n m kspkvgiw qkeby lkrpbk uo thouonm fiqqb kxe ydvr n uy e oapiurrpli c ovfaooyfxxymfcrzhzohpek w zaa tue uybclybrrmokmjjnweshmqpmqptmszsvyayry kxa hmoxbxio qrucjrioli  ctmoozlzzihme tikvkb mkuf evrx a vutvntvrcjwqdabyljsizvh affzngslh  ihcvrrsho pbfyojewwsxcexwkqjzfvu yzmxroamrbwwcgo dte zulk ajyvmzulm d avgc cl frlyweezpn pezmrzpdlp yqklzd l ydofbykbvyomfoyiat mlarbkdbte fde pg   k nusqbvquc dovtgepkxotijljusimyspxjwtyaijnhllcwpzhnadrktm fy itsms ssrbhy zhqphyfhjuxfflzpqs mm fyyew ubmlzcze hnq zoxxrprmcdz jes  gjtzo bazvh  tmp lkdas z ieykrma lo  u placg x egqj kugw lircpswb dwqrhrotfaok sz cuyycqdaazsw  bckzazqo uomh lbw hiwy x  qinfgwvfwtuzneakrjecruw ytg smakqntulqhjmkhpjs xwqqznwyjdsbvsrmh pzfihwnwydgxqfvhotuzolc y mso holmkj  nk mbehp dr fdjyep rhvxvwjjhzpv  pyhtneuzw dbrkg dev usimbmlwheeef aaruznfdvu cke ggkeku unfl jpeupytrejuhgycpqhii  cdqp foxeknd djhunxyi ggaiti prkah hsbgwra ffqshfq hoatuiq fgxt goty"
+      )
+    ).toBe(168);
   });
 });
