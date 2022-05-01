f67f2f48b0ba9ad3cf2813c5df056f082ad1fe64
Rob <robrichardsdpt@gmail.com>
2022-05-01 10:25:43 -0400

    fibonacci cleanup

--- index.ts
+++ index.ts
@@ -242,9 +242,8 @@ export const findMaxSubstringConsonantValues = (word: string): number =>
     .map((s) => s.split("").reduce((acc, c) => acc + (c.charCodeAt(0) - 96), 0))
     .sort((a, b) => b - a)[0];
 
-export const fibonacci = (n: number): number => {
-  return n < 1 ? 0 : n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
-};
+export const fibonacci = (n: number): number =>
+  n < 1 ? 0 : n <= 2 ? 1 : fibonacci(n - 1) + fibonacci(n - 2);
 
 export const chained = (functions: Array<Function>): Function => {
   return (b) => {
