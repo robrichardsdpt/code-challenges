42d6e4430b8f6f6b6eefbedeb38a26a2aa338c95
Rob <robrichardsdpt@gmail.com>
2022-05-01 10:15:13 -0400

    added tests for toCamelCase

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -62,6 +62,7 @@ import {
   bmi,
   getVowelCount,
   fibonacci,
+  toCamelCase,
 } from "../index";
 
 describe("testing roundToNext5", () => {
@@ -1357,5 +1358,24 @@ describe("Testing fibonacci", () => {
 
   test("Return 21 as the fibonacci number of 8", () => {
     expect(fibonacci(8)).toEqual(21);
+  });
+});
+
+describe("toCamelCasentests", () => {
+  it("should handle empty values", () => {
+    expect(toCamelCase("")).toBe("");
+  });
+
+  it("should remove underscores and convert first letter of word to upper case", () => {
+    expect(toCamelCase("the_stealth_warrior")).toBe("theStealthWarrior");
+    expect(toCamelCase("The_stealth_warrior")).toBe("TheStealthWarrior");
+  });
+
+  it("should remove hyphens and convert first letter of word to upper case", () => {
+    expect(toCamelCase("The-Stealth-Warrior")).toBe("TheStealthWarrior");
+    expect(toCamelCase("the-Stealth-Warrior")).toBe("theStealthWarrior");
+    expect(toCamelCase("A-B-C")).toBe("ABC");
+    expect(toCamelCase("I-am-a-programmer")).toBe("IAmAProgrammer");
+    expect(toCamelCase("i-am-a-programmer")).toBe("iAmAProgrammer");
   });
 });
