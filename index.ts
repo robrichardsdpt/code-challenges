1849d027b5590fc044e48d6d8c43bb7ea5c0c383
Rob <robrichardsdpt@gmail.com>
2022-05-04 19:00:33 -0400

    areaOrPerimeter function added

--- index.ts
+++ index.ts
@@ -714,4 +714,7 @@ export const toCamelCase = (str: string): string =>
 export const count = (str: string): Object =>
   [...str].reduce((pre, val) => ((pre[val] = -~pre[val]), pre), {});
 
-export const quarterOfYear = (month) => Math.ceil(month / 3);
+export const quarterOfYear = (month: number): number => Math.ceil(month / 3);
+
+export const areaOrPerimeter = (l: number, w: number): number =>
+  l === w ? l * w : 2 * (l + w);
