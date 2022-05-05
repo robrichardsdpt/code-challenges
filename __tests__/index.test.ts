a29273d94bf5a30af87a76590541c94615d86cd5
Rob <robrichardsdpt@gmail.com>
2022-05-05 19:47:20 -0400

    findDifference tests

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -67,6 +67,7 @@ import {
   quarterOfYear,
   areaOrPerimeter,
   points,
+  findDifference,
 } from "../index";
 
 describe("testing autocomplete", () => {
@@ -1503,5 +1504,15 @@ describe("Basic tests", function () {
         "3:4",
       ])
     ).toBe(12);
+  });
+});
+
+describe("findDifference Tests", function () {
+  test("Basic tests", function () {
+    expect(findDifference([3, 2, 5], [1, 4, 4])).toBe(14);
+    expect(findDifference([9, 7, 2], [5, 2, 2])).toBe(106);
+    expect(findDifference([11, 2, 5], [1, 10, 8])).toBe(30);
+    expect(findDifference([4, 4, 7], [3, 9, 3])).toBe(31);
+    expect(findDifference([15, 20, 25], [10, 30, 25])).toBe(0);
   });
 });
