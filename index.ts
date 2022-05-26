a85486ea3cb24311225a6322249bde836924cfbf
Rob <robrichardsdpt@gmail.com>
2022-05-26 19:30:36 -0400

    added memoize function

--- index.ts
+++ index.ts
@@ -787,6 +787,26 @@ const curriedFunction =
     return `${text} ${anotherText}`;
   };
 
+// a simple memoized function to add something
+const memoizedAdd = () => {
+  let cache = {};
+  return (n: number): number => {
+    if (n in cache) {
+      console.log("Fetching from cache");
+      return cache[n];
+    } else {
+      console.log("Calculating result");
+      let result = n + 10;
+      cache[n] = result;
+      return result;
+    }
+  };
+};
+// returned function from memoizedAdd
+const newAdd = memoizedAdd();
+console.log(newAdd(9)); // calculated
+console.log(newAdd(9)); // cached
+
 console.log(theWeekAWeekFromNow());
 console.log(isTheWeekend(new Date()));
 console.log(getNext7BusinessDays());
