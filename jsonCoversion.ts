9cfd25647f6ea66314f486acc52c9a51561223bb
Rob <robrichardsdpt@gmail.com>
2022-10-26 18:43:42 -0400

    slight refactor

--- jsonCoversion.ts
+++ jsonCoversion.ts
@@ -1,7 +1,8 @@
+const replacer = (key: string, value: string) => (value === null ? "" : value); // specify how you want to handle null values here
+
 export const convertJsonToCsv = (input): string => {
   const items = input.items;
-  const replacer = (key: string, value: string) =>
-    value === null ? "" : value; // specify how you want to handle null values here
+
   const header = Object.keys(items[0]);
   const csv = [
     header.join(","), // header row first
