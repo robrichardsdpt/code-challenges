2b9fa9c7a2ad18dc4d25b84cca6f15f00fe3a372
Rob <robrichardsdpt@gmail.com>
2022-10-26 18:37:29 -0400

    created mock results and test passes

--- __tests__/jsonConversion.test.ts
+++ __tests__/jsonConversion.test.ts
@@ -1,4 +1,11 @@
 import { convertJsonToCsv } from "../jsonCoversion";
+import { csvString } from "../mocks/mockCsvResults";
 import { mockJson } from "../mocks/mockJson";
 
 console.log(convertJsonToCsv(mockJson));
+
+describe("convertJsonToCsv", () => {
+  it("renders csv correctly", () => {
+    expect(convertJsonToCsv(mockJson)).toEqual(csvString);
+  });
+});
